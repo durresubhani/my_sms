3fcf47b2e4ad977eae5b3c4fef7dc8f838762f67
duresubhani <duresubhani1@gmail.com>
2023-10-03 21:36:48 +0500

    User Module Product Module

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { ProductsModule } from './products/products.module';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [UsersModule,ProductsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
